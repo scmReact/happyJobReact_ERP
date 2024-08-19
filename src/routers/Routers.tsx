7d781e4760d86b718e7f748b0290c6925560bc1c
jxxone <wao_14@naver.com>
2024-08-19 10:47:42 +0900

    승진내역관리 상세조회 추가

--- src/routers/Routers.tsx
+++ src/routers/Routers.tsx
@@ -2,6 +2,7 @@ import { RouteObject, createBrowserRouter } from "react-router-dom";
 import { Login } from "../pages/Login";
 import { DashBoard } from "../component/layout/DashBoard/DashBoard";
 import { NotFound } from "../component/common/NotFound/NotFound";
+import { EmpGrade } from "../pages/Employee/EmpGrade";
 
 const routers: RouteObject[] = [
     { path: "*", element: <NotFound /> },
@@ -9,7 +10,13 @@ const routers: RouteObject[] = [
     {
         path: "/react",
         element: <DashBoard />,
-        children: [],
+        children: [
+            { path: 'employee', 
+                children : [
+                    { path:'empGrade.do', element: <EmpGrade/> }
+                ]
+            }
+        ],
     },
 ];
 
