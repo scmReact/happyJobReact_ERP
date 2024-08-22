3af96e9fed41c8a94b2a32d98051401646d5e10a
verde1105 <verde1105@lunaflo@naver.com>
2024-08-22 14:18:12 +0900

    24-08-22

--- src/routers/Routers.tsx
+++ src/routers/Routers.tsx
@@ -2,7 +2,7 @@ import { RouteObject, createBrowserRouter } from "react-router-dom";
 import { Login } from "../pages/Login";
 import { DashBoard } from "../component/layout/DashBoard/DashBoard";
 import { NotFound } from "../component/common/NotFound/NotFound";
-import { EmpSalePlan } from "../pages/Business/EmpSalePlan/EmpSalePlan";
+
 
 import { EmpGrade } from "../pages/Employee/EmpGrade";
 import { Unpaid } from "../pages/Accounting/Unpaid";
@@ -10,7 +10,9 @@ import { BizPartner } from "../pages/Business/BizPartner";
 import { EstMng } from "../pages/Business/EstMng";
 import { VctnApprove } from "../pages/Business/VctnApprove";
 import { VctnCalendar } from "../pages/Employee/VctnCalendar";
+import { EmpSalePlan } from "../pages/Business/EmpSalePlan/EmpSalePlan";
 import { DailyRevenue } from "../pages/Sales/DailyRevenue";
+
 
 const routers: RouteObject[] = [
   { path: "*", element: <NotFound /> },
@@ -22,45 +24,30 @@ const routers: RouteObject[] = [
       {
         path: "employee",
         children: [
-            { path: 'employee', 
-                children : [
-                    { path:'empGrade.do', element: <EmpGrade/> },
-                    { path: "empGrade.do", element: <EmpGrade /> },
-                    { path: "vctnApprove.do", element: <VctnApprove /> },
-                    { path: "vctnCalendar.do", element: <VctnCalendar /> },
-                ]
-            },
-            {
-                path: "accounting",
-                children: [{ path: "unpaid.do", element: <Unpaid /> }],
-            },
-            {
-                path: "business",
-                    children: [{ path: "bizPartner.do", element: <BizPartner/> },
-                        { path: "estMng.do", element: <EstMng /> }
-                    ],
-            },
-            {
-                path: "sales",
-                    children:[{path: "dailyRevenue.do", element: <DailyRevenue/>}]
-            },
-            
-          ],
-        },
-        {
-          path: "accounting",
-          children: [{ path: "unpaid.do", element: <Unpaid /> }],
-        },
-        {
-          path: "business",
-          children: [
-            { path: "bizPartner.do", element: <BizPartner /> },
-            { path: "estMng.do", element: <EstMng /> },
-            { path: "empSalePlan.do", element: <EmpSalePlan /> },
-          ],
-        },
-      ],
-    },
-  ];
+          { path: "empGrade.do", element: <EmpGrade /> },
+          { path: "vctnApprove.do", element: <VctnApprove /> },
+          { path: "vctnCalendar.do", element: <VctnCalendar /> },
+        ],
+      },
+      {
+        path: "accounting",
+        children: [{ path: "unpaid.do", element: <Unpaid /> }],
+      },
+      {
+        path: "business",
+        children: [
+          { path: "bizPartner.do", element: <BizPartner /> },
+          { path: "estMng.do", element: <EstMng /> },
+          { path: "empSalePlan.do", element: <EmpSalePlan /> },
+          
+        ],
+      },
+      {
+        path: "sales",
+            children:[{path: "dailyRevenue.do", element: <DailyRevenue/>}]
+      },
+    ],
+  },
+];
 
 export const Routers = createBrowserRouter(routers);
