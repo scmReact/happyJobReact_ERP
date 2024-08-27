7f7d35ee107d8eaf748aafc2efe1b42e55c38712
InitTester <Initsave@gmail.com>
2024-08-27 09:48:18 +0900

    중복 제거

--- src/common/formatWon.ts
+++ src/common/formatWon.ts
@@ -1,12 +1,5 @@
+export const formatWon = (price: number) => {
+    const price1 = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-export const formatWon = (price : number) => {
-
-    const price1 = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") ;
-
-    return `${price1}`+`원`;
+    return `${price1}` + `원`;
 };
-
-export const formatWon = (value: number) => {
-    return `${new Intl.NumberFormat('ko-KR').format(value)} 원`;
-};
-
